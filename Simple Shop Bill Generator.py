c4ea21ca66b6e5cf8ce6431f001cd7d639d26c7a
Thisum Gamage <thisumgmg@gmail.com>
2026-04-09 22:30:54 +0530

    Finished

--- Simple Shop Bill Generator.py
+++ Simple Shop Bill Generator.py
@@ -1,50 +1,64 @@
-def Sub_Total():
-
-    Total = sum()
-    return Total
+import datetime
 
 
-def Discount():
+def sub_total(item_list):
 
-    if Sub_Total() > 10000:
-        Discount = 15 / 100
-    elif Sub_Total() > 5000:
-        Discount = 10 / 100
+    total = 0
+    for item in item_list:
+        total = total + item[1] * item[2]
+    return total
+
+
+def discount_calc(item_list):
+
+    current_total = sub_total(item_list)
+    if current_total > 10000:
+        discount = current_total * (15 / 100)
+    elif current_total > 5000:
+        discount = current_total * (10 / 100)
     else:
-        Discount = 0
-    return Discount
+        discount = 0
+    return discount
 
 
-def Final_Amount():
+def final_amount(item_list):
 
-    Net_Amount = Sub_Total() - Discount()
-    return Net_Amount
+    net_amount = sub_total(item_list) - discount_calc(item_list)
+    return net_amount
 
 
 print("--Welcome To Simple Shop Bill Generator--")
 print("-----------------------------------------")
 
+item_list = []
 
 while True:
 
-    Item_Name = input("Enter Your Item Name : ")
-    Quantity = float(input("Enter Your Quantity : "))
-    Price_Per_Item = float(input("Enter Price Per Item : "))
+    item_name = input("Enter Your Item Name\t: ")
+    quantity = float(input("Enter Your Quantity\t: "))
+    price_per_item = float(input("Enter Price Per Item\t: "))
 
-    user_choice = input("\nAre you add another item ? (Y/N) : ")
+    user_record = [item_name, quantity, price_per_item]
+    item_list.append(user_record)
+    print("-----------------------------------------")
+    user_choice = input("Are you add another item ? (Y/N) : ")
+    print("-----------------------------------------")
     if user_choice.upper() == "N":
         break
 
+current_time = datetime.datetime.now().strftime("%Y-%m-%d  %I:%M %p")
 
+print("\n--------This is Your Bill Summary--------")
+print(f"Date & Time : {current_time}")
+print("\nItem Name\tQuantity\tPrice(Rs.)")
 print("-----------------------------------------")
 
-print("--------This is Your Bill Summary--------")
+for item in item_list:
 
-print("\nItem Name\t: ")
-print("Quantity\t: ")
-print("Price\t\t: ")
-print("Total\t\t: ")
-print("Discount\t: ")
-print("Final Amount\t: ")
+    print(f"\n{item[0]}\t\t{item[1]}\t\t{item[1]*item[2]:.2f}")
+
+print(f"\nTotal\t\t: Rs. {sub_total(item_list):.2f}")
+print(f"Discount\t: Rs. {discount_calc(item_list):.2f}")
+print(f"Final Amount\t: Rs. {final_amount(item_list):.2f}")
 
 print("\n----------------Thank You----------------")
